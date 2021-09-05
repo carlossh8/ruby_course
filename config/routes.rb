96b9361c120c9704aea1a49b3817548eb337d1c8
carlossh8 <carlos.sh8@outlook.com>
2021-09-05 10:39:09 -0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get "produtos",to: "produtos#index"
-  post "produtos", to: "produtos#create"
-  get "produtos/new", to: "produtos#new"
+  resources :produtos, only: [:new, :create, :destroy]
   root to: "produtos#index"
 end
